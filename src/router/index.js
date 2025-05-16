12d71be1c37bd88cfdca28163cd3d11100ad7985
xiaofan <ningfengzeng@gmail.com>
2025-05-16 10:37:39 +0800

    chore: update route style

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createWebHistory, createRouter } from 'vue-router'
+import {createWebHistory, createRouter, createWebHashHistory} from 'vue-router'
 /* Layout */
 import Layout from '@/layout'
 
@@ -111,7 +111,7 @@ export const dynamicRoutes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes: constantRoutes,
   scrollBehavior(to, from, savedPosition) {
     if (savedPosition) {
